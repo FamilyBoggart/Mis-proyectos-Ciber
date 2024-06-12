a9ff73a780e1288c76622d5a857bfb2802febbc2
FamilyBoggart <alexanderoga@gmail.com>
2024-06-12 16:39:43 +0200

    BYTE_TO_DIG FUNCTION ADDDED
    The script can now transform dig to bytes and viceversa, also there are
    a case menu to work with different method calling of this script

--- Utils/data_conversor.sh
+++ Utils/data_conversor.sh
@@ -1,12 +1,13 @@
 # OK
-dig_to_byte()
+function dig_to_byte()
 {
 	local n=128
 	local number=$1
-	local bit=""
+	local bit
 
 	while [ $n -gt 0 ]
 	do
+
 		if [ $number -ge $n ]
 		then
 			number=$(($number - $n))
@@ -14,8 +15,32 @@ dig_to_byte()
 		else
 			bit+="0"
 		fi
-		((n/=2))
+		n=$(($n / 2))
 	done
 	echo $bit
 }
 
+function byte_to_dig {
+
+	local bin_array=$1
+	local num=0
+	local n=128
+	local bit
+
+	for((i=0;i<8;i++))
+	do
+		bit=${bin_array:$i:1}
+		if [[ $bit == "1" ]]
+		then
+			num=$(($num + $n))
+		fi
+			n=$(($n / 2))
+	done
+	echo $num
+} 
+
+case $1 in
+	"dig_to_byte") dig_to_byte $2 ;;
+	"byte_to_dig") byte_to_dig $2 ;;
+	"*") exit 0 
+esac
